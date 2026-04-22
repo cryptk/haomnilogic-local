f26780bb1cf59219779e53484e02e878ec8a2a15
Chris Jowett <421501+cryptk@users.noreply.github.com>
2026-04-22 00:12:59 +0000

    fix: update library to resolve infinite wait while waiting for response

--- custom_components/omnilogic_local/coordinator.py
+++ custom_components/omnilogic_local/coordinator.py
@@ -6,7 +6,6 @@ import logging
 from typing import TYPE_CHECKING
 
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
-from pyomnilogic_local.api.exceptions import OmniFragmentationError, OmniMessageFormatError, OmniTimeoutError, OmniValidationError
 
 from .const import SCAN_INTERVAL
 
@@ -51,15 +50,7 @@ class OmniLogicCoordinator(DataUpdateCoordinator[None]):
         """Update data via library."""
         try:
             await self.omni.refresh(force=True)
-        except OmniFragmentationError as err:
-            self.failure_counts["connection"] = self.failure_counts.get("connection", 0) + 1
-            raise UpdateFailed("Failure to fragment or reassemble data to/from OmniLogic") from err
-        except OmniMessageFormatError as err:
-            self.failure_counts["format"] = self.failure_counts.get("format", 0) + 1
-            raise UpdateFailed("Received a malformed or invalid message from OmniLogic") from err
-        except OmniTimeoutError as err:
-            self.failure_counts["timeout"] = self.failure_counts.get("timeout", 0) + 1
-            raise UpdateFailed("Timeout occurred while fetching OmniLogic data") from err
-        except OmniValidationError as err:
-            self.failure_counts["validation"] = self.failure_counts.get("validation", 0) + 1
-            raise UpdateFailed("Received invalid data from OmniLogic") from err
+        except Exception as err:
+            errName = type(err).__name__
+            self.failure_counts[errName] = self.failure_counts.get(errName, 0) + 1
+            raise UpdateFailed("Failed to update data from OmniLogic") from err
